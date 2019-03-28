3ab77286fa9e72eab682dd19c97c16931097f6d4
Willie Verger <ingverger@gmail.com>
2019-03-28 17:16:12 -0300

    Version 2.0.3 - Racionalizacion de la cantidad de niveles de dificultad
    Version 2.0.3 - Racionalizacion de la cantidad de niveles de dificultad.

---
+++ gen-memorioso-apk.bat
@@ -0,0 +1,4 @@
+pgb update 3414713
+pgb unlock android 361772 key_password=termopilas keystore_password=termopilas
+pgb build 3414713
+pgb download 3414713 android e:\Dropbox\GitHub\Memorioso\.privado\apk\
